d900f378a5f9522ee2b61704ff405cae9e240211
Katy_Zhe <nekhaeva.e@yandex.ru>
2023-08-28 18:32:58 +0300

    TypeScript added for index, App, FullPizza

--- src/components/FullPizza/FullPizza.tsx
+++ src/components/FullPizza/FullPizza.tsx
@@ -2,8 +2,8 @@ import React from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import axios from "axios";
 
-export default function FullPizza() {
-  const [pizza, setPizza] = React.useState();
+const FullPizza: React.FC = () => {
+  const [pizza, setPizza] = React.useState<{imageURL: string, title: string, price: number}>();
   const { id } = useParams();
   const navigate = useNavigate();
   React.useEffect(() => {
@@ -21,12 +21,15 @@ export default function FullPizza() {
     fechPizza();
   }, []);
   if (!pizza) {
-    return "Loading...";
+    return <>"Loading..."</>;
   }
   return (
     <div>
-      <h1>FullPizza</h1>
+      <img src={pizza.imageURL} alt="картинка с пиццей" />
       <h2>{pizza.title}</h2>
+      <h4>{pizza.price}</h4>
     </div>
   );
 }
+
+export default FullPizza;
